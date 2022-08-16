df82422822eeb25d6b90d4ae37e5cae26b3bcf27
Fabio Sabariego Rodrigues <fabiosabariego87@hotmail.com>
2022-08-16 11:54:41 -0300

    Primeiro Exercicio

---
+++ Exercicios/StringsManipulation/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace StringsManipulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringForFloat = "0,85"; // datatype should be float
+            string stringForInt = "12345"; // datatype should be int
+
+            Console.WriteLine($"Float: {float.Parse(stringForFloat)}");
+            Console.WriteLine($"Integer: {int.Parse(stringForInt)}");
+            Console.ReadKey();
+        }
+    }
+}
