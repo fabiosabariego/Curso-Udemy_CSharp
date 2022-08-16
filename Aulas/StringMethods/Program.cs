3f2151a0cf020fb82b730947b1aa691a218d33d4
Fabio Sabariego Rodrigues <fabiosabariego87@hotmail.com>
2022-08-16 17:45:23 -0300

    Aula sobre String

---
+++ Aulas/StringMethods/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace StringMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // ToLower()
+            // ToUpper()
+            // Trim()
+            // Substring()
+            // IndexOf()
+            // IsNullOrWhiteSpace()
+            // StringFormat
+        }
+    }
+}
