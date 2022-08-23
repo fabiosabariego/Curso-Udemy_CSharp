e4c9c9faf8cd624f9433b078a631369b5360c6a7
Fabio Sabariego Rodrigues <fabiosabariego87@hotmail.com>
2022-08-23 08:01:30 -0300

    For Loop
    Primeira Aula e Desafio de For Loop

---
+++ Aulas/ForLoop/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ForLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            // Usando Condicao For para Loop -- Muito Usado para Contagem
+            for(int counter = 0; counter < 10; counter++)
+            {
+                Console.WriteLine($"Valor do Loop: {counter}");
+            }
+            Console.WriteLine("");
+
+            //--------------------------------------------------------
+            for (int counter = 0; counter < 30; counter+=5)
+            {
+                Console.WriteLine($"Valor do Loop: {counter}");
+            }*/
+
+            // Desafio: Imprimir valores de 0 a 50, somente os pares!!
+            for (int num = 0; num <= 20; num++)
+            {
+                if (num % 2 == 0)
+                {
+                    Console.WriteLine($"O numero {num} e Par!!!");
+                }
+            }
+
+            Console.WriteLine("");
+            // Ou tambem podemos fazer assim, para numeros impares!:
+            for (int num = 1; num <= 20; num += 2)
+            {
+                Console.WriteLine($"O numero {num} e Impar!!!");
+            }
+        }
+    }
+}
