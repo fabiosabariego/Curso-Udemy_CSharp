d372c5c7b614d90013bf92f77a80e597e2619392
Fabio Sabariego Rodrigues <fabiosabariego87@hotmail.com>
2022-08-23 07:42:30 -0300

    Aulas e Ex
    Finalizando o Capitulo de Condicoes If / Else (Cap 4)

---
+++ Exercicios/Ex12_TernaryOperator/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Ex12_TernaryOperator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Criando um valor de temperatura
+            int temp = 0;
+
+            string tempMsg = string.Empty;
+            string inputValue = string.Empty;
+
+            Console.Write("Digite uma Temperatura: ");
+            inputValue = Console.ReadLine();
+
+            bool validTemp = int.TryParse(inputValue, out temp);
+
+            // Primeira Verificacao: Se a temperatura e do tipo Int
+            /*
+            if (validTemp)
+            {
+                Console.WriteLine("A Variavel e do tipo Int!\n");
+                if (temp <= 15)
+                {
+                    Console.WriteLine("Esta muito frio Aqui!!");
+                }
+                else if (temp >= 16 && temp <= 28)
+                {
+                    Console.WriteLine("Temperatura Ok");
+                }
+                else if (temp > 28)
+                {
+                    Console.WriteLine("Esta muito quente aqui!!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Temperatura nao compativel!");
+            }
+            */
+
+            // Outra forma de realizar a condicao
+            if (validTemp)
+            {
+                tempMsg = temp <= 15 ? "Esta muito Frio Aqui" : (temp >= 16 && temp <= 28) ? "Temperatura esta Ok" : temp > 28 ? "Esta muito quente aquI!": "";
+                Console.WriteLine(tempMsg);
+            }
+            else
+            {
+                Console.WriteLine("Temperatura nao compativel!");
+            }
+
+        }
+    }
+}
