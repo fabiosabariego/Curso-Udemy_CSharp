8a85f8c88572030c686f1238b11ab3ab94755c14
Fabio Sabariego Rodrigues <fabiosabariego87@hotmail.com>
2022-11-01 07:29:40 -0300

    Polimorfismo Exercicios
    Exercicio para coletar palavras chaves dentro de um texto e montar uma mensagem escondida!

---
+++ Exercicios/Ex32_ReadWriteFile/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.IO;
+
+namespace Ex32_ReadWriteFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Coletando dados do arquivo e gerando um novo arquivo!!");
+            HiddenWords();
+            
+        }
+
+        static public void HiddenWords()
+        {
+            string link = @"C:\Users\Fabio\Documents\Aulas C#\CursoUdemy\Curso-Udemy_CSharp\Assets\";
+            string[] textLines = File.ReadAllLines(link + "input.txt");
+            string[] splitTextLines;
+
+            using(StreamWriter file = new StreamWriter(link + "hidden.txt"))
+            {
+                foreach (string lines in textLines)
+                {
+                    if (lines.Contains("split") || lines.Contains("Split") || lines.Contains("SPLIT"))
+                    {
+                        splitTextLines = lines.Split();
+                        file.WriteLine(splitTextLines[4]);
+                    }
+                }
+            }
+
+            Console.WriteLine("Coleta completa com sucesso!!!");
+        }
+    }
+}
