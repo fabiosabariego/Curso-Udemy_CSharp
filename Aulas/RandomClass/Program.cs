f280b827b7d55beb6cd699e4ae9a8646548f880f
Fabio Sabariego Rodrigues <fabiosabariego87@hotmail.com>
2022-11-04 07:59:31 -0300

    Regular Expression Class

---
+++ Aulas/RandomClass/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace RandomClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random dice = new Random();
+            int numEyes;
+
+            for (int i = 0; i < 10; i++)
+            {
+                numEyes = dice.Next(1, 4);      // Gera numeros aleatorios entre 1 e 3
+                
+                if (numEyes == 1)
+                {
+                    Console.WriteLine("YES!!!");
+                }
+                else if (numEyes == 2)
+                {
+                    Console.WriteLine("MAYBE!!");
+                }
+                else if (numEyes == 3)
+                {
+                    Console.WriteLine("NO!!");
+                }
+            }
+
+        }
+    }
+}
---
+++ Aulas/RegularExpressions/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RegularExpressions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
