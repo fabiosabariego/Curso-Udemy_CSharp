ca41c9e62e3f85ea3d8dda43e2287282d3792243
Fabio Sabariego Rodrigues <fabiosabariego87@hotmail.com>
2022-11-08 06:42:42 -0300

    main args class

---
+++ Aulas/MainArgs/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace MainArgs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Verificando se existem alguma coisa criada no args, pois se nao houver, quando chamamos a variavel args[0] gera um erro
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Nao existem nada criado no args, criar algo para nao gerar falha!");
+                return;
+            }
+            
+            if (args[0].ToLower() == "help")
+            {
+                Console.WriteLine("******************* INSTRUCTIONS *******************");
+                Console.WriteLine("* Use one of the following commands followed by 2 numbers");
+                Console.WriteLine("* 'add' to add 2 numbers");
+                Console.WriteLine("* 'sub' to subtract 2 numbers");
+                Console.WriteLine("****************************************************");
+                return;
+            }
+            
+        }
+    }
+}
