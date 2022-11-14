fb3afe50ac75ee703914f9fe206af42e389e0897
Fabio Sabariego Rodrigues <fabiosabariego87@hotmail.com>
2022-11-14 07:54:38 -0300

    Delegate and Lambda

--- Aulas/DelegatesCreating/Program.cs
+++ Aulas/DelegatesCreating/Program.cs
@@ -23,6 +23,46 @@
             Console.WriteLine("");
 
             DisplayPeople("Senior", people, IsSenior);
+            Console.WriteLine("");
+
+
+            // ANONYMOUS METHOD ****************************************************************
+            // here we created a new variable called filter of the type FilterDelegate
+            // then we assigned an anonymous method to it instead of an already defined method
+            FilterDelegate filter = delegate (Person p)
+            {
+                return p.Age >= 20 && p.Age <= 30;
+            };
+
+            DisplayPeople("Between 20 & 30", people, filter);
+            Console.WriteLine("");
+
+
+            // Display all people
+            DisplayPeople("All", people, delegate (Person p) { return true; });
+            Console.WriteLine("");
+            // *********************************************************************************
+
+            // LAMBDA EXPRESSION (=>) ##########################################################
+            // Statement Labda
+            string searchKeyword = "A";
+            DisplayPeople("age > 20 with search keyword: " + searchKeyword, people, (p) =>
+            {
+                if(p.Name.Contains(searchKeyword) && p.Age > 20)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            });
+            Console.WriteLine("");
+
+            // Expression Lambda
+            DisplayPeople("exactly 25", people, p => p.Age == 25);
+
+            // #################################################################################
 
         }
 
@@ -60,5 +100,7 @@
         {
             return p.Age >= 65;
         }
+
+
     }
 }
---
+++ Exercicios/LambdaExpression/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace LambdaExpression
+{
+    internal class Program
+    {
+        // Criando as Funcoes Matematicas
+        static Func<float, float, float> Add = (a,b) => a + b;
+        static Func<float, float, float> Sub = (a, b) => a - b;
+        static Func<float, float, float> Mul = (a, b) => a * b;
+        static Func<float, float, float> Div = (a, b) => a / b;
+
+        static void Main(string[] args)
+        {
+            Dictionary <string, Func<float, float, float>> Operators = new Dictionary<string, Func<float, float, float>>
+            {
+                {"+", Add},
+                {"-", Sub},
+                {"*", Mul},
+                {"/", Div}
+            };
+
+            public static Func<float, float, float> OperationGet(string s)
+            {
+                if (Operators.ContainsKey(s))
+                {
+                    return Operators[s];
+                }
+                return null;
+            }
+        }
+
+        
+    }
+}
