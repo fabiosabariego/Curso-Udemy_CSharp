50d9148571cb74acbb297a9a17cf07adefce5005
Fabio Sabariego Rodrigues <fabiosabariego87@hotmail.com>
2022-11-23 06:59:49 -0300

    WPF CLasses

---
+++ Aulas/RoutedEvents/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RoutedEvents
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Button Direct Event");
+        }
+
+        private void Button_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Button Bubbling Event");
+        }
+
+        private void Button_PreviewMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Button Tunneling Event");
+        }
+    }
+}
--- Aulas/XAMLBasics_Code/MainWindow.xaml.cs
+++ Aulas/XAMLBasics_Code/MainWindow.xaml.cs
@@ -23,6 +23,33 @@ namespace XAMLBasics_Code
         public MainWindow()
         {
             InitializeComponent();
+
+            Grid grid = new Grid();                     // Cria o Grid para a Tela
+            this.Content = grid;
+
+            Button btn = new Button();                  // Cria o Botao para acionamento na Tela grafica
+            btn.FontSize = 26;                          // Define o tamanho da fonte do botao para 26
+            btn.Height = 100;                           // Define as dimensoes do botao
+            btn.Width = 200;                            // Define as dimensoes do botao
+
+            WrapPanel wrapPanel = new WrapPanel();      // Cria o WrapPanel (para podermos adicionar varias caracteristicas ao botao
+            TextBlock txt = new TextBlock();            // Define o Texto que ira aparecer no botao
+            txt.Text = "Multi";                         // Texto do Botao
+            txt.Foreground = Brushes.Blue;              // Define a cor do texto do botao
+            wrapPanel.Children.Add(txt);                // Adiciona o texto ao wrap panel (primeira parte do texto
+
+            txt = new TextBlock();
+            txt.Text = "Color";
+            txt.Foreground = Brushes.Red;
+            wrapPanel.Children.Add(txt);
+
+            txt = new TextBlock();
+            txt.Text = "Button";
+            txt.Foreground = Brushes.Green;
+            wrapPanel.Children.Add(txt);
+
+            btn.Content = wrapPanel;                    // Adiciona o WrapPanel com todos os textos e cores definidos ao botao
+            grid.Children.Add(btn);                     // Adiciona o botao ao Grid da Tela
         }
     }
 }
